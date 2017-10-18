cc9ba46209a8bf7566131ba7240a0f37c99c8ec4
Geoff Harcourt <geoff.harcourt@gmail.com>
2017-10-17 20:10:55 -0400

    Add run scoring

--- lib/grit.rb
+++ lib/grit.rb
@@ -7,6 +7,14 @@ class Grit
   end
 
   def generate_simulated_plate_appearance
+    batting_line_outcome.merge(run_outcome)
+  end
+
+  private
+
+  attr_reader :random_number_generator, :stat_line
+
+  def batting_line_outcome
     case random_event
     when 0..h_1b_rate
       { ab: 1, h: 1, tb: 1 }
@@ -14,8 +22,8 @@ class Grit
       { ab: 1, h: 1, tb: 2 }
     when (h_1b_rate + h_2b_rate)..(h_1b_rate + h_2b_rate + h_3b_rate)
       { ab: 1, h: 1, tb: 3 }
-    when (h_1b_rate + h_2b_rate + h_3b_rate)..(h_1b_rate + h_2b_rate + h_3b_rate + hr_rate)
-      { ab: 1, h: 1, tb: 4, hr: 1 }
+    when home_run_range
+      { ab: 1, h: 1, tb: 4, hr: 1, r: 1 }
     when h_rate..(h_rate + bb_rate)
       { ab: 0, bb: 1 }
     when (h_rate + bb_rate)..(h_rate + bb_rate + hbp_rate)
@@ -23,13 +31,33 @@ class Grit
     when on_base_percentage..(on_base_percentage + non_ab_out_rate)
       { ab: 0, bb: 0, h: 0, hbp: 0 }
     else
-      {}
+      { ab: 1, r: 0 }
     end
   end
 
-  private
+  def home_run_range
+    (h_1b_rate + h_2b_rate + h_3b_rate)..(h_1b_rate + h_2b_rate + h_3b_rate + hr_rate)
+  end
 
-  attr_reader :random_number_generator, :stat_line
+  def run_outcome
+    case random_event
+    when home_run_range
+      { r: 1 }
+    when 0..on_base_percentage
+      { r: run_value }
+    else
+      { r: 0 }
+    end
+  end
+
+  def run_value
+    if random_number_generator.rand < (stat_line[:r] - stat_line[:hr]) /
+        (stat_line[:h] + stat_line[:bb] + stat_line[:hbp] - stat_line[:hr]).to_f
+      1
+    else
+      0
+    end
+  end
 
   def random_event
     @_random_event = random_number_generator.rand
--- spec/grit_spec.rb
+++ spec/grit_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe Grit do
         hr: 4,
         bb: 25,
         hbp: 1,
+        r: 20,
       }
     end
 
@@ -75,7 +76,68 @@ RSpec.describe Grit do
         )
 
         expect(simulator.generate_simulated_plate_appearance).
-          to match(a_hash_including(ab: 1, h: 1, tb: 4, hr: 1))
+          to match(a_hash_including(ab: 1, h: 1, tb: 4, hr: 1, r: 1))
+      end
+    end
+
+    context "when there's a non-base-reaching event" do
+      it "does not generate an R result" do
+        random_number_generator = instance_double(Random, rand: 1.0)
+
+        simulator = Grit.new(
+          stat_line: stat_line,
+          random_number_generator: random_number_generator,
+        )
+
+        expect(simulator.generate_simulated_plate_appearance).
+          to match(a_hash_including(ab: 1, r: 0))
+      end
+    end
+
+    context "when there's an on-base event" do
+      context "and the event is run-scoring" do
+        it "generates an R result" do
+          random_number_generator = instance_double(Random)
+          allow(random_number_generator).to receive(:rand).and_return(1.0, 0.34)
+
+          simulator = Grit.new(
+            stat_line: stat_line,
+            random_number_generator: random_number_generator,
+          )
+
+          expect(simulator.generate_simulated_plate_appearance).
+            to match(a_hash_including(r: 1))
+        end
+      end
+
+      context "and the event is bats in runs" do
+        it "generates an R result" do
+          random_number_generator = instance_double(Random)
+          allow(random_number_generator).to receive(:rand).and_return(1.0, 0.34)
+
+          simulator = Grit.new(
+            stat_line: stat_line,
+            random_number_generator: random_number_generator,
+          )
+
+          expect(simulator.generate_simulated_plate_appearance).
+            to match(a_hash_including(r: 1))
+        end
+      end
+
+      context "and the event is not run-scoring" do
+        it "does not generate an R result" do
+          random_number_generator = instance_double(Random)
+          allow(random_number_generator).to receive(:rand).and_return(1.0, 0.35)
+
+          simulator = Grit.new(
+            stat_line: stat_line,
+            random_number_generator: random_number_generator,
+          )
+
+          expect(simulator.generate_simulated_plate_appearance).
+            to match(a_hash_including(r: 0))
+        end
       end
     end
 
@@ -108,7 +170,21 @@ RSpec.describe Grit do
     end
 
     context "when the event is between the HBP threshold and the non-AB pa out threshold" do
-      it "generates a HBP result" do
+      it "generates a PA, non-AB result" do
+        random_number_generator = instance_double(Random, rand: 0.52)
+
+        simulator = Grit.new(
+          stat_line: stat_line,
+          random_number_generator: random_number_generator,
+        )
+
+        expect(simulator.generate_simulated_plate_appearance).
+          to match(a_hash_including(ab: 0, h: 0, bb: 0, hbp: 0))
+      end
+    end
+
+    context "when the event is between the HBP threshold and the non-AB pa out threshold" do
+      it "generates a PA, non-AB result" do
         random_number_generator = instance_double(Random, rand: 0.52)
 
         simulator = Grit.new(
